4a506c2e9b6373c3cf973b0758703ca31354919b
sinemihlali0017 <sinemihlalimahlathini@gmail.com>
2025-10-07 17:33:12 +0200

    washiza

--- scripts/house_of_the_lord.js
+++ scripts/house_of_the_lord.js
@@ -1,6 +1,14 @@
-document.getElementById("last-updated").textContent =
-  "Last updated: " + new Intl.DateTimeFormat("en-GB", {
-    dateStyle: "full",
-    timeStyle: "medium"
-  }).format(new Date());
 
+
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastmodified").textContent = document.lastModified;
+
+const menuButton = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuButton.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+  
+  menuButton.textContent = navMenu.classList.contains("show") ? "✖" : "☰";
+});
