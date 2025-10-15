a021dc608f8974787799291d825c1f9ab6b95cb8
sinemihlali0017 <sinemihlalimahlathini@gmail.com>
2025-10-15 11:43:17 +0200

    yay

--- scripts/form.js
+++ scripts/form.js
@@ -1,10 +1,12 @@
 
+
+
 const products = [
-  { id: "fc-1888", name: "flux capacitor", averagerating: 4.5 },
-  { id: "fc-2050", name: "power laces", averagerating: 4.7 },
-  { id: "fs-1987", name: "time circuits", averagerating: 3.5 },
-  { id: "ac-2000", name: "low voltage reactor", averagerating: 3.9 },
-  { id: "jj-1969", name: "warp equalizer", averagerating: 5.0 }
+  { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+  { id: "fc-2050", name: "Power Laces", averagerating: 4.7 },
+  { id: "fs-1987", name: "Time Circuits", averagerating: 3.5 },
+  { id: "ac-2000", name: "Low Voltage Reactor", averagerating: 3.9 },
+  { id: "jj-1969", name: "Warp Equalizer", averagerating: 5.0 }
 ];
 
 
@@ -16,6 +18,24 @@ products.forEach(product => {
   select.appendChild(option);
 });
 
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastmodified').textContent = document.lastModified;
 
- document.getElementById('currentyear').textContent = new Date().getFullYear();
-    document.getElementById('lastmodified').textContent = document.lastModified;
+
+
+const form = document.getElementById("reviewForm");
+
+form.addEventListener("submit", function() {
+  
+  let reviewCount = localStorage.getItem("reviewCount");
+
+  if (reviewCount === null) {
+    reviewCount = 0;
+  }
+
+  
+  reviewCount++;
+
+  
+  localStorage.setItem("reviewCount", reviewCount);
+});
